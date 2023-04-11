fc51655a26aeca8bb243d1c2eda9e58d91d2ff50
Steve <vkhanhqui@gmail.com>
2023-04-11 09:49:35 +0700

    FEAT: Add Commands, and Events

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/LikePostCmd.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Commands;
+
+namespace Post.Cmd.Api.Commands
+{
+    public class LikePostCmd : BaseCommand
+    {
+    }
+}
---
+++ SM-Post/Post.Common/Events/CommentAddedEvent.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    public class CommentAddedEvent : BaseEvent
+    {
+        public CommentAddedEvent(): base(nameof(CommentAddedEvent))
+        {
+
+        }
+
+        public Guid CommentId {get;set;}
+        public string Comment {get;set;}
+        public string Username {get;set;}
+        public DateTime CommentDate {get;set;}
+    }
+}
