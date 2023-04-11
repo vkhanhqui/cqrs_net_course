3bcae1b0ee637fe3f7b1e9ab4c407c3a901027b4
Steve <vkhanhqui@gmail.com>
2023-04-11 10:28:25 +0700

    FEAT: Add CommandDispatcher following Mediator Pattern

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly Dictionary<Type, Func<BaseCommand, Task>> _handlers = new();
+
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if(_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("Could not register the same command handler");
+            }
+            _handlers.Add(typeof(T), x => handler((T) x)); // Cast BaseCommand (x) to specific Command (T)
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if(_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new ArgumentNullException(nameof(handler), "No command handler was registed");
+            }
+        }
+    }
+}
