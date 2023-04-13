7f145aa9e649fbe0d812b7365482b37f282def39
Steve <vkhanhqui@gmail.com>
2023-04-13 14:51:47 +0700

    FEAT: Add EventConsumer

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,10 +1,15 @@
+using Confluent.Kafka;
+using CQRS.Core.Consumers;
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 Action<DbContextOptionsBuilder> configureDbContext = ( // Read configs from appsettings.Development.json
     o => o.UseLazyLoadingProxies()
         .UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
@@ -14,6 +19,13 @@ builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory
 // create database and tables
 var dataContext = builder.Services.BuildServiceProvider().GetRequiredService<DatabaseContext>();
 dataContext.Database.EnsureCreated();
+
+// Add services to the container.
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IEventHandler, Post.Query.Infrastructure.Handlers.EventHandler>();
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+builder.Services.AddScoped<IEventConsumer, EventConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
